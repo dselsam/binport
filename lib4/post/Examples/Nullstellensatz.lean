f4c5476b91122204c0908507d7c4cf9c7ec599f1
Daniel Selsam <daniel.selsam@protonmail.com>
2021-01-28 15:57:37 -0800

    example: some defs from nullstellensatz

---
+++ lib4/post/Examples/Nullstellensatz.lean
@@ -0,0 +1,49 @@
+import Mathlib.ring_theory.jacobson
+import Mathlib.field_theory.algebraic_closure
+import Mathlib.field_theory.mv_polynomial
+import Mathlib.algebraic_geometry.prime_spectrum
+
+namespace Mathlib
+
+syntax "{ " ident (" : " term)? " | " term " }" : term
+
+macro_rules
+  | `({ $x : $type | $p }) => `(Mathlib.set_of fun ($x:ident : $type) => $p)
+  | `({ $x | $p })         => `(Mathlib.set_of fun ($x:ident : _) => $p)
+
+-- TODO: need to set `:max` when porting singletons
+notation:max "⊤" => has_top.top
+notation:max "⊥" => has_bot.bot
+
+universes u v w
+
+noncomputable instance {α : Type u} [has_zero α] : OfNat α (noindex! 0) := ⟨has_zero.zero⟩
+noncomputable instance {α : Type u} [has_one α]  : OfNat α (noindex! 1) := ⟨has_one.one⟩
+
+open ideal
+
+namespace mv_polynomial
+
+variable {k : Type u} [field k]
+variable {σ : Type v}
+
+noncomputable def zero_locus (I : ideal (mv_polynomial σ k)) : set (σ → k) :=
+  { x : σ → k | ∀ (p : mv_polynomial σ k), p ∈ I →
+     coe_fn (_inst_1 := ring_hom.has_coe_to_fun) (eval x) p = 0 }
+
+theorem mv_polynomial.mem_zero_locus_iff {I : ideal (mv_polynomial σ k)} {x : σ → k} :
+    x ∈ zero_locus I ↔ (p : mv_polynomial σ k) → p ∈ I → coe_fn (_inst_1 := Mathlib.ring_hom.has_coe_to_fun) (eval x) p = has_zero.zero :=
+  sorry
+
+theorem zero_locus_anti_mono {I J : ideal (mv_polynomial σ k)} (h : I ≤ J) : zero_locus J ≤ zero_locus I :=
+  sorry
+
+theorem zero_locus_bot : zero_locus (⊥ : ideal (mv_polynomial σ k)) = ⊤ :=
+  sorry
+
+theorem zero_locus_top : zero_locus (⊤ : ideal (mv_polynomial σ k)) = ⊥ :=
+  sorry
+
+
+end mv_polynomial
+end Mathlib
