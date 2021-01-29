aba639948221efd75f3f01a6b5ada0ec7591ea87
Daniel Selsam <daniel.selsam@protonmail.com>
2021-01-28 18:56:36 -0800

    example: some work on spaces

---
+++ lib4/post/Examples/Spaces.lean
@@ -0,0 +1,43 @@
+import Mathlib.topology.opens
+import Mathlib.category_theory.sites.grothendieck
+import Mathlib.category_theory.sites.pretopology
+import Mathlib.category_theory.limits.lattice
+
+namespace Mathlib
+
+universe u
+
+namespace opens
+
+variable (T : Type u) [topological_space T]
+
+open category_theory topological_space category_theory.limits
+
+noncomputable def grothendieck_topology : @grothendieck_topology (opens T) (preorder.small_category _) := {
+  sieves := λ (X : topological_space.opens T) (S : category_theory.sieve X) =>
+      (x : T) → x ∈ X → ∃ (U : topological_space.opens T), ∃ (f : U ⟶ X),
+        -- TODO: this is a recurring issue and we must solve it, one way or another
+        @coe_fn _ sieve.has_coe_to_fun S U f ∧ x ∈ U,
+
+  top_mem' := λ (X : topological_space.opens T) (x : T) (hx : x ∈ X) =>
+    Exists.intro X (Exists.intro (category_theory.category_struct.id X) { left := trivial, right := hx }),
+
+  pullback_stable' := sorry,
+  transitive' := sorry
+}
+
+-- TODO: the *type* of the following definition triggers the error of Meta/SynthInstance.lean:L193
+-- which means that some instance that gets tried is not recognized as a class.
+-- This is expected since we are currently discarding `@[class] def ...` annotations entirely
+-- (Lean4 only permits structures to be classes)
+#exit
+noncomputable def pretopology : @pretopology (opens T) (preorder.small_category _) sorry := {
+  coverings := λ (X : topological_space.opens T) (R : category_theory.presieve X) =>
+      (x : T) → x ∈ X → Exists λ (U : topological_space.opens T) => Exists λ (f : U ⟶ X) => R f ∧ x ∈ U,
+  has_isos := sorry,
+  pullbacks := sorry,
+  transitive := sorry
+}
+
+end opens
+end Mathlib
