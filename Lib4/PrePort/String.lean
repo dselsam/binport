c68ed44a6fad28a4fc5c3f6c9fef63a77d4e2de2
Daniel Selsam <daniel.selsam@protonmail.com>
2021-02-11 12:30:18 -0800

    missing file

---
+++ Lib4/PrePort/String.lean
@@ -0,0 +1,57 @@
+/-
+Copyright (c) 2020 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Daniel Selsam
+-/
+import PrePort.Number
+
+namespace Mathlib
+namespace PrePort
+
+def Fin (n : Nat) := {i : Nat // i < n}
+
+inductive Nat.LessThanOrEqual (a : Nat) : Nat → Prop
+| refl : LessThanOrEqual a a
+| step : ∀ {b}, LessThanOrEqual a b → LessThanOrEqual a b.succ
+
+@[reducible] protected def Nat.le (n m : Nat) := LessThanOrEqual n m
+@[reducible] protected def Nat.lt (n m : Nat) := LessThanOrEqual n.succ m
+
+abbrev isValidChar (n : Nat) : Prop :=
+  Nat.lt n 0xd800 ∨ (Nat.lt 0xdfff n ∧ Nat.lt n 0x110000)
+
+structure Char where
+  val   : Nat
+  valid : isValidChar val
+
+def toChar3 (c : _root_.Char) : Char := {
+  val   := c.val.val.val,
+  valid := sorry
+}
+
+def fromChar3 (c : Char) : _root_.Char := {
+  val   := ⟨⟨c.val, sorry⟩⟩,
+  valid := sorry
+}
+
+structure String where
+  data : List Char
+
+def toString3 : _root_.String → String
+  | ⟨data⟩ => ⟨data.map toChar3⟩
+
+section ListAll
+
+variable {α β : Type} {p : α → Bool}
+
+def fromString3 (s : String) : _root_.String :=
+  ⟨s.data.map fromChar3⟩
+
+end ListAll
+
+#check @String
+#check @toString3
+#check @fromString3
+
+end PrePort
+end Mathlib
