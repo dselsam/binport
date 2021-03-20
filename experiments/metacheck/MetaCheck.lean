d3563c7185e70e2348d05ae760260b9d7fdd388f
Daniel Selsam <daniel.selsam@protonmail.com>
2021-03-20 08:33:25 -0700

    experiment: starting meta-check experiment

---
+++ experiments/metacheck/MetaCheck.lean
@@ -0,0 +1,119 @@
+/-
+Copyright (c) 2021 Microsoft Corporation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Daniel Selsam
+-/
+import Lean
+import Std.Data.HashSet
+
+open Lean Lean.Meta
+open Std (HashSet mkHashSet)
+
+namespace MetaCheckExperiment
+
+def BLACK_LIST : HashSet Name :=
+  ({} : HashSet Name)
+
+inductive MetaCheckResult where
+  | success      : MetaCheckResult
+  | failed       : MetaCheckResult
+  | timeout      : MetaCheckResult
+  | other        : MetaCheckResult
+  deriving Inhabited
+
+instance : ToString MetaCheckResult := ⟨fun
+  | MetaCheckResult.success      => "success"
+  | MetaCheckResult.failed       => "failed"
+  | MetaCheckResult.timeout      => "timeout"
+  | MetaCheckResult.other        => "other"
+⟩
+
+structure DataPoint where
+  declName : Name
+  result   : MetaCheckResult
+
+abbrev Job := Task (Except IO.Error (Array DataPoint))
+
+instance : Inhabited Job := ⟨⟨pure #[]⟩⟩
+
+structure Context where
+
+structure State where
+  datapoints : Array DataPoint := #[]
+
+abbrev MetaCheckExperimentM := ReaderT Context (StateRefT State MetaM)
+
+def emit (datapoint : DataPoint) : MetaCheckExperimentM Unit :=
+  modify fun s => { s with datapoints := s.datapoints.push datapoint }
+
+def checkTypeValue (declName : Name) (type value : Expr) : MetaCheckExperimentM Unit := withTransparency TransparencyMode.all $ do
+  try
+    let valueType ← inferType value
+    let b ← isDefEq type valueType
+    match b with
+    | true  => emit { declName := declName, result := MetaCheckResult.success }
+    | false => emit { declName := declName, result := MetaCheckResult.failed }
+  catch ex =>
+    let msg ← ex.toMessageData.toString
+    println! "[warn:isDefEq] {msg}"
+    emit { declName := declName, result := MetaCheckResult.other }
+
+def checkConstant (env : Environment) (opts : Options) (cinfo : ConstantInfo) : IO (Array DataPoint) := do
+  let ((_, s), _, _) ← MetaM.toIO ((core.run {}).run {}) { options := opts } { env := env }
+  pure s.datapoints
+
+  where
+    core : MetaCheckExperimentM (Array DataPoint) := do
+      match cinfo.value? with
+      | some value => checkTypeValue cinfo.name cinfo.type value
+      | _ => pure ()
+      (← get).datapoints
+
+def runMetaCheckExperiment (env : Environment) (opts : Options) : IO Unit := do
+  let jobs ← env.constants.map₁.foldM (init := #[]) collectJobs
+  let jobs ← env.constants.map₂.foldlM (init := jobs) collectJobs
+  IO.FS.withFile "results.csv" IO.FS.Mode.write fun handle => do
+    println! "-- waiting for {jobs.size} jobs --"
+    let mut i := 0
+    for (declName, job) in jobs do
+      println! "[{i}] {declName}"
+      (← IO.getStdout).flush
+      i := i + 1
+      match ← IO.wait job with
+      | Except.ok datapoints => dumpResults handle datapoints
+      | Except.error err => println! "[warn:task] {err}"
+
+  where
+    collectJobs (jobs : Array (Name × Job)) (name : Name) (cinfo : ConstantInfo) : IO (Array (Name × Job)) := do
+      if BLACK_LIST.contains name then return jobs
+      if isPrivateName name then return jobs
+      if name.isInternal then return jobs
+      if not ((`Mathlib).isPrefixOf name) then return jobs
+      jobs.push (name, ← IO.asTask $ checkConstant env opts cinfo)
+
+    dumpResults handle datapoints := do
+      for ⟨declName, result⟩ in datapoints do
+        handle.putStr s!"{declName} {toString result}\n"
+
+end MetaCheckExperiment
+
+open MetaCheckExperiment
+
+unsafe def withEnvOpts {α : Type} (f : Environment → Options → IO α) : IO α := do
+  initSearchPath s!"../../lean4/build/release/stage1/lib/lean:../../Lib4"
+
+  let opts : Options := {}
+  -- let opts : Options := opts.insert `maxHeartbeats (DataValue.ofNat 1000)
+  -- let opts : Options := opts.insert `synthInstance.maxHeartbeats (DataValue.ofNat 50000)
+
+  let imports : List Import := [
+    { module := `Init : Import },
+    { module := `PrePort : Import },
+    -- { module := `Lean3Lib.init.data.nat.basic : Import },
+    { module := `Mathlib.all : Import },
+    { module := `PostPort : Import }
+  ]
+
+  withImportModules imports (opts := opts) (trustLevel := 0) fun env => f env opts
+
+unsafe def main : IO Unit := withEnvOpts runMetaCheckExperiment
---
+++ experiments/metacheck/analyze.py
@@ -0,0 +1,42 @@
+import csv
+import collections
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', action='store', type=str)
+args = parser.parse_args()
+
+by_class = collections.defaultdict(list)
+
+totals = collections.defaultdict(int)
+
+with open(args.filename) as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in reader:
+        if len(row) < 2: continue
+        clsName, result = row[0], row[1]
+        by_class[clsName].append(result)
+        totals[result] += 1
+
+by_class_avgs = {}
+for clsName, results in by_class.items():
+    by_class_avgs[clsName] = sum([1 if result == "success" else 0 for result in results]) / len(results)
+
+classes = list(by_class.keys())
+classes.sort(key=(lambda c: -by_class_avgs[c]))
+
+n_total         = 0
+n_success_total = 0
+
+for clsName in classes:
+    n   = len(by_class[clsName])
+    avg = by_class_avgs[clsName]
+    print("%80s %5d %0.2f" % (clsName, n, avg))
+
+    n_total += n
+    n_success_total += n * avg
+
+print("----------")
+print("Total: (%d / %d) = %0.2f" % (round(n_success_total), n_total, n_success_total / n_total))
+for status, total in totals.items():
+    print("  %16s: %3d (%0.2f)" % (status, total, total / n_total))
