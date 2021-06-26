0fe2b85c2f78a3ee73f000da7d856f7a344b166d
Daniel Selsam <daniel.selsam@protonmail.com>
2021-06-26 12:09:19 -0700

    chore: rm _placeholder_

--- Lib4/PrePort.lean
+++ Lib4/PrePort.lean
@@ -4,12 +4,3 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Daniel Selsam
 -/
 import PrePort.Numbers
-
--- TODO: put elsewhere
-axiom _placeholder_ (α : Prop) : α
-
-@[appUnexpander _placeholder_] def unexpandPlaceholder : Lean.PrettyPrinter.Unexpander
-  | `(_placeholder_ $prop:term) => `(?_)
-  | _                           => throw ()
-
-#check _placeholder_ (2 + 2 = 4)
